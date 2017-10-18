44ec867e14298c419d44ef17728a5e383b28f0b1
yr-imac <691047223@qq.com>
2017-10-18 11:28:41 +0800

    init

---
+++ javViewer/javViewer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  javViewer
+//
+//  Created by   yr-imac on 2017/10/17.
+//  Copyright © 2017年   yr-imac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ javViewer/javViewer/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  javViewer
+//
+//  Created by   yr-imac on 2017/10/17.
+//  Copyright © 2017年   yr-imac. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
